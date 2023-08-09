4c9b2de11a21c5d25b9373834a84d9bf1906bac6
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-09 10:51:52 -0700

    magic calculation

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,8 +2,5 @@
 def magic_calculation(a, b, c):
     if a < b:
         return c
-    if c > b:
+    elif c > b:
         return a * b - c
-
-    result = magic_calculation(-1, 0, 3)
-    print(result)
