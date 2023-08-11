75e3d3b137ead2e1dc007c709d725ca8ca60d04e
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-11 10:43:54 -0700

    magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
-
     if a < b:
-        c = add(a, b)
+        c = magic_calculation_102.add(a, b)
         for i in range(4, 6):
-            c = add(c, i)
+            c = magic_calculation_102.add(c, i)
         return c
     else:
-        return sub(a, b)
+        return magic_calculation_102.sub(a, b)
