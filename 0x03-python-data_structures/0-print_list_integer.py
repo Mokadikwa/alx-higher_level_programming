1adeb99f49af90a469698e6e6fcae1713f307925
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-14 06:57:12 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        formatted_item = str(item) 
-        print(formatted_item)
+    for num in my_list:
+        print("{}".format(num))
