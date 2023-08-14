98005f6ff7113c000e50691bad4d16722bcd2c35
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-14 11:16:51 -0700

    task 6 fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            end = " " if num != row[-1] else ""
+            print("{:d}".format(num), end=end)
         print()
