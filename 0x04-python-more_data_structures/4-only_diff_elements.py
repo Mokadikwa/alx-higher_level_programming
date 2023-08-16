66e85d09dce89d29e1631a3c8d482a0f35462624
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-16 07:30:53 -0700

    task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    unique_to_set_1 = set_1.difference(set_2)
+    unique_to_set_2 = set_2.difference(set_1)
+    result_set = unique_to_set_1.union(unique_to_set_2)
+    return result_set
