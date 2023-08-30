42ff0ae53154e30ea0a614945e5fc3c06f5a7643
Mokadikwa <mpmokadikwa@gmail.com>
2023-08-30 05:53:28 -0700

    0x06 Python

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """initialize a new square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method for the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method to set it"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout with char '#'"""
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print('#' * self.size)
