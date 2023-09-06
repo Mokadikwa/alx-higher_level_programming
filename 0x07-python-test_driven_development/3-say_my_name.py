c66e74259f752e136486a62b4a9fb312584ca78e
Mokadikwa <mpmokadikwa@gmail.com>
2023-09-06 06:18:35 -0700

    tasts

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
-    """prints first_nameand last_name"""
+    """
+    Prints "My name is <first_name> <last_name>".
 
+    :param first_name: The first name.
+    :type first_name: str
+    :param last_name: The last name (optional).
+    :type last_name: str
+    :raises TypeError: If `first_name` is not a string or if both `first_name` and `last_name` are not provided.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
+    if not first_name and not last_name:
+        raise TypeError("At least one name must be provided.")
     if last_name:
-        print(f"{first_name} {last_name}")
+        print(f"My name is {first_name} {last_name}")
     else:
-        print(first_name)
+        print(f"My name is {first_name}")
